ec5332d1592e59471a815fe8e5e2e0e2aa2bf0df
shahazad08 <sk.shahazad@gmail.com>
2022-04-22 14:53:22 +0530

    Added: Addition of 2 number functionality added

--- src/com/bridgelabz/MathsOperation.java
+++ src/com/bridgelabz/MathsOperation.java
@@ -3,5 +3,8 @@ package com.bridgelabz;
 public class MathsOperation {
     public static void main(String args[]) {
         System.out.println("Welcome to Programming");
+        int a=10, b=20, c;
+        c=a+b;
+        System.out.println("Addition is" +c);
     }
 }
