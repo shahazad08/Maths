7b1d44def03097e717ade3e41138c74e39336d07
shahazad08 <sk.shahazad@gmail.com>
2022-04-22 14:59:10 +0530

    Added: Subtraction code is added

--- src/com/bridgelabz/MathsOperation.java
+++ src/com/bridgelabz/MathsOperation.java
@@ -3,8 +3,10 @@ package com.bridgelabz;
 public class MathsOperation {
     public static void main(String args[]) {
         System.out.println("Welcome to Programming");
-        int a=10, b=20, c;
+        int a=10, b=20, c, z;
         c=a+b;
+        z=a-b;
         System.out.println("Addition is" +c);
+        System.out.println("Subtraction is" +z);
     }
 }
